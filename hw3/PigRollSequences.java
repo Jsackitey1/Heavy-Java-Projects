07e94e3a3de5901c8c37b6fbe436551624728ca7
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2025-02-04 22:14:09 -0500

    FT

--- hw3/PigRollSequences.java
+++ hw3/PigRollSequences.java
@@ -1,32 +1,49 @@
-
 public class PigRollSequences {
-	private static int [] e;
-	
+	private static int[] e;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println(getNumSequences(50));
+		System.out.println(getNumSequences(50));
 
 	}
+
+	public static int getNumSequences(int turnTotal) {
+		
+//		if(turnTotal==0) {
+//	        return 1;
+//	    }
+//	    
+//	    if(turnTotal<0) {
+//	        return 0;
+//	    }
+//	    
+//	    int total = 0;
+//	    
+//	    for (int i = 2; i <= 6; i++) {
+//	        total += getNumSequences(turnTotal - i);
+//	    }
+//	    
+//	    return total;
 	
-	public static int getNumSequences(int turnTotal){
-		
-		if(turnTotal==0) {
-			return 1;
-		}
-		
-		if(turnTotal<0) {
+		if (turnTotal < 0) {
+			
 			return 0;
 		}
-		
-		int total = 0;
-		
-        for (int i = 2; i <= 6; i++) {
-        	
-            total += getNumSequences(turnTotal - i);
-        }
-        
-        return total;
+
+		// Create array to store results 
+		int[] dp = new int[turnTotal + 1];
+		dp[0] = 1; 
+
+		// Calculate sequences for each possible total from 1 to turnTotal
+		for (int i = 1; i <= turnTotal; i++) {
+			for (int dice = 2; dice <= 6; dice++) {
+				if (i >= dice) {
+					dp[i] += dp[i - dice];
+				}
+			}
+		}
+
+		return dp[turnTotal];
 	}
 
 }
--- hw3/ToadsAndFrogsSolver.java
+++ hw3/ToadsAndFrogsSolver.java
@@ -14,35 +14,89 @@ public class ToadsAndFrogsSolver {
 		in.close();
 	}
 
+	
 	public static char getWinner(char[] board, char currentPlayer) {
-		// TODO - implement
 		
-		// Algorithm: 
-		// For each legal move for the current player,
-		//   - if we call getWinner on the resulting game state (new board, other player)
-		//     and find that this leads to a win for the current player, 
-		//     then return the current player.
-		// Otherwise, failing to find a winning play (or any legal play), return the opponent player.
-		
-		
-		
-		
-		/*getWinner(board, current player):
-
-		for each board position:
-		if the current player is 'T' and a 'T' is at this position:
-		if a move is not out of bounds and there is an empty space to the right:
-		make the move in the board
-		char winner = getWinner(board, 'F')
-		unmake the move in the board
-		if winner == current player, return current player
-		if a hop is not out of bounds and there is a 'F' to the right and a '-' to the right of that:
-		... (same structure as with move)
-		else (the current player is 'F' and 'F' is at this position):
-		... (same structure as with current player 'T')
-		return opponent player  (Note: We only reach this point if either (BASE CASE) no legal moves were found, or (RECURSIVE CASE) all legal moves were wins for the opponent.)*/
-
-		return board[0];
+	    char opponent = (currentPlayer == TOAD) ? FROG : TOAD;
+	    
+	    //for each board position:
+	    
+	    for (int i = 0; i < board.length; i++) {
+	    	//if the current player is 'T' and a 'T' is at this position:
+	        if (board[i] == currentPlayer) {
+	            if (currentPlayer == TOAD) {
+	            	//if a move is not out of bounds and there is an empty space to the right:
+	                if (i + 1 < board.length && board[i + 1] == EMPTY) {
+	                    board[i] = EMPTY;
+	                    board[i + 1] = TOAD;
+//	                    make the move in the board
+//	                    char winner = getWinner(board, 'F')
+	                    
+	                    char winner = getWinner(board, opponent);
+	                    
+	                    board[i] = TOAD;
+	                    board[i + 1] = EMPTY;
+//	                    if winner == current player, return current player
+	                    if (winner == currentPlayer) {
+	                        return currentPlayer;
+	                    }
+	                }
+//	                if a hop is not out of bounds and there is a 'F' to the right and a '-' to the right of that:
+//	                ... (same structure as with move)
+	                if (i + 2 < board.length && board[i + 1] == FROG && board[i + 2] == EMPTY) {
+	                    board[i] = EMPTY;
+	                    board[i + 2] = TOAD;
+	                    
+	                    char winner = getWinner(board, opponent);
+	                    
+	                    board[i] = TOAD;
+	                    board[i + 2] = EMPTY;
+	                    
+	                    if (winner == currentPlayer) {
+	                        return currentPlayer;
+	                    }
+	                }
+	            }
+//	            else (the current player is 'F' and 'F' is at this position):
+//	        		... (same structure as with current player 'T')
+	            else {
+	                if (i - 1 >= 0 && board[i - 1] == EMPTY) {
+	                    board[i] = EMPTY;
+	                    board[i - 1] = FROG;
+	                    
+	                    char winner = getWinner(board, opponent);
+	                    
+	                    board[i] = FROG;
+	                    board[i - 1] = EMPTY;
+	                    
+	                    if (winner == currentPlayer) {
+	                        return currentPlayer;
+	                    }
+	                }
+	                
+	                // Check hop left over opponent
+	                if (i - 2 >= 0 && board[i - 1] == TOAD && board[i - 2] == EMPTY) {
+	                    // Make hop
+	                    board[i] = EMPTY;
+	                    board[i - 2] = FROG;
+	                    
+	                    // Recursive check
+	                    char winner = getWinner(board, opponent);
+	                    
+	                    // Unmake hop
+	                    board[i] = FROG;
+	                    board[i - 2] = EMPTY;
+	                    
+	                    if (winner == currentPlayer) {
+	                        return currentPlayer;
+	                    }
+	                }
+	            }
+	        }
+	    }
+	    
+	    // If no winning moves found, return opponent
+	    return board[0];
 	}
 
 }
