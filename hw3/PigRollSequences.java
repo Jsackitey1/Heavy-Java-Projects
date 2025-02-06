3dca57012c170869c27bc62fef80309c558473c2
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2025-02-06 00:21:36 -0500

    second

--- hw3/PigRollSequences.java
+++ hw3/PigRollSequences.java
@@ -16,7 +16,7 @@ public class PigRollSequences {
 //	    if(turnTotal<0) {
 //	        return 0;
 //	    }
-//	    
+//	   
 //	    int total = 0;
 //	    
 //	    for (int i = 2; i <= 6; i++) {
@@ -35,6 +35,7 @@ public class PigRollSequences {
 		dp[0] = 1; 
 
 		// Calculate sequences for each possible total from 1 to turnTotal
+		
 		for (int i = 1; i <= turnTotal; i++) {
 			for (int dice = 2; dice <= 6; dice++) {
 				if (i >= dice) {
