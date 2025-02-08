0a73016d6023b80095b927cf394554db69b7e250
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2025-02-08 18:08:56 -0500

    ex2 f1

--- ex2/MyPoint2D.java
+++ ex2/MyPoint2D.java
@@ -1,7 +1,7 @@
 
 public class MyPoint2D {
-	static int x;
-	static int y;
+	static double x;
+	static double y;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,22 +9,40 @@ public class MyPoint2D {
 	}
 	
 	public MyPoint2D(double x, double y) {
+		this.x=x;
+		this.y=y;
 		
 	}
 	
 	public double getDistance(double x,double y) {
 		
-		return 1.1;
+		return Math.sqrt(Math.pow(this.x-x,2) + Math.pow(this.y-y,2));
 	}
 	
 	public double getDistance(MyPoint2D other) {
 		
-		return 0.1;
+		return Math.sqrt(Math.pow(this.x-other.getX(),2) + Math.pow(this.y-other.getY(),2));
 	}
 	
-	public String toString(){
-		
-		return "";
+	@Override
+	public String toString() {
+		return String.format("(%s, %s)",MyPoint2D.getX(), MyPoint2D.getY());
+	}
+
+	public static double getX() {
+		return x;
+	}
+
+	public static void setX(int x) {
+		MyPoint2D.x = x;
+	}
+
+	public static double getY() {
+		return y;
+	}
+
+	public static void setY(int y) {
+		MyPoint2D.y = y;
 	}
 
 }
--- ex2/NearestNeighborPoint2D.java
+++ ex2/NearestNeighborPoint2D.java
@@ -1,29 +1,54 @@
+import java.util.ArrayList;
 
 public class NearestNeighborPoint2D {
-	
-	
+
+	private ArrayList<MyPoint2D> points; 
+
 	public NearestNeighborPoint2D() {
-		
+		points = new ArrayList<>(); 
 	}
-	
+
 	public void add(MyPoint2D point) {
-		
+		if (point != null) {
+			points.add(point);
+		}
 	}
-	
+
 	public MyPoint2D getNearestNeighbor(MyPoint2D point) {
+		if (points.isEmpty() || point == null) {
+			return null;
+		}
 		
-		return new MyPoint2D(0,0);
+
+		MyPoint2D nearestDist = points.get(0);
+		double minDistance = point.getDistance(nearestDist);
+
+		for (MyPoint2D p : points) {
+			double distance = point.getDistance(p);
+			if (distance < minDistance) {
+				minDistance = distance;
+				nearestDist = p;
+			}
+		}
+
+		return nearestDist;
 	}
-	
-	public MyPoint2D getNearestNeighbor(double x,double y) {
+
+	public MyPoint2D getNearestNeighbor(double x, double y) {
+		if (points.isEmpty()) {
+			return null;
+		}
+
+		MyPoint2D tempPoint = new MyPoint2D(x, y);
 		
-		return new MyPoint2D(0,0);
-		
+		return getNearestNeighbor(tempPoint);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+
+	
 
 }
