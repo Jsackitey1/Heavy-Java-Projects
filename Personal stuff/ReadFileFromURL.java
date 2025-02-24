a9bb6e4734aa307b47314fcb0842542501a25d53
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2025-02-24 02:27:45 -0500

    crawler

---
+++ Personal stuff/ReadFileFromURL.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ReadFileFromURL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a url: ");
+		
+		String URLString= new Scanner(System.in).next();
+		try {
+			URL url =new URL(URLString);
+			int count=0;
+			Scanner input= new Scanner(url.openStream());
+			while(input.hasNext()) {
+				String line=input.nextLine();
+				count+=line.length();
+				
+			}
+			System.out.println("The file size is "+ count+" characters");
+			
+			
+		}
+		
+		catch(MalformedURLException e) {
+			System.out.println("invalid url.");
+			
+		}
+		catch(IOException e) {
+			System.out.println("i/o errors: no such file exist.");
+		}
+
+	}
+
+}
