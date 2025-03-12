2854956e56d6b7239f82417f8ff4f564f1f9f1c9
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2025-03-11 22:36:28 -0400

    ex4

---
+++ ex4/MakeAJoke.java
@@ -0,0 +1,39 @@
+
+public class MakeAJoke {
+	
+	
+	public static void main(java.lang.String[] args) {
+		
+	}
+	
+	public MakeAJoke() {
+		
+	}
+	
+	public boolean loadJokes(java.lang.String filename) {
+		return false;
+	}
+	
+	public java.lang.String getJoke(){
+		return "";
+	}
+	
+	public java.lang.String getJoke(int index){
+		return "";
+	}
+	
+	public void addJoke(java.lang.String joke) {
+		
+	}
+	
+	public int size() {
+		return 1;
+	}
+	
+	public boolean saveJokes(java.lang.String filename){
+		return false;
+	}
+	
+	
+
+}
